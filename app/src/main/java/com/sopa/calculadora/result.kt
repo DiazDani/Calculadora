e61647d094a6e42ca43caf17fbe1292f53e1bd4e
dani.diaz <daniel.diaz@institutvidreres.cat>
2023-11-06 14:30:38 +0100

    DATABINDINGS

--- app/src/main/java/com/sopa/calculadora/result.kt
+++ app/src/main/java/com/sopa/calculadora/result.kt
@@ -2,8 +2,11 @@ package com.sopa.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sopa.calculadora.databinding.ActivityResultBinding
 
 class result : AppCompatActivity() {
+
+    lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
