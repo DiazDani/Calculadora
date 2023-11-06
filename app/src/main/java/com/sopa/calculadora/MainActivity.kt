24936bcbd7e3dd82b76d411f0a7a3804f7f267d5
dani.diaz <daniel.diaz@institutvidreres.cat>
2023-11-06 09:11:04 +0100

    Initial commit

---
+++ app/src/main/java/com/sopa/calculadora/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sopa.calculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sopa.calculadora.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
